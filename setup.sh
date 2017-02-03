c89f6bee75a1efa2e550b39fda6d309bda8a8676
Brennan Spellacy <brennanspellacy@gmail.com>
2017-02-03 18:35:54 -0500

    Removed unneeded files

--- setup.sh
+++ setup.sh
@@ -161,13 +161,9 @@ echo "done"
 #
 
 declare -a FILES_TO_SYMLINK=(
-
-  'shell/shell_aliases'
-  'shell/shell_config'
   'shell/shell_exports'
   'shell/shell_functions'
   'shell/bash_profile'
-  'shell/bash_prompt'
   'shell/bashrc'
   'shell/zshrc'
   'shell/ackrc'
@@ -175,11 +171,9 @@ declare -a FILES_TO_SYMLINK=(
   'shell/gemrc'
   'shell/inputrc'
   'shell/screenrc'
-
   'git/gitattributes'
   'git/gitconfig'
   'git/gitignore'
-
 )
 
 # FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin" # add in vim and the binaries
@@ -329,8 +323,8 @@ ln -s ~/dotfiles/zsh/themes/nick.zsh-theme $HOME/.oh-my-zsh/themes
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Install the Solarized Dark theme for iTerm
-open "${HOME}/dotfiles/iterm/themes/Solarized Dark.itermcolors"
+# Install one-dark to match Atom
+open "${HOME}/code/dotfiles/iterm/themes/one-dark.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
